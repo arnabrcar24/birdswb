ae75e712300ac8db14df66eb08267f5e4b6b5a57
Arnab <arnabrc24@gmail.com>
2024-06-27 17:44:11 +0530

    a5

--- src/app/services/bird.service.ts
+++ src/app/services/bird.service.ts
@@ -8,15 +8,15 @@ import { Bird } from '../type/bird';
 export class BirdService {
   constructor(private http:HttpClient) {}
   getBird(){
-    return this.http.get("http://localhost:3000/birds")
+    return this.http.get("https://667d5510297972455f64a257.mockapi.io/api/v1/birds")
   }
   addBird(birddata:Bird){
-    return this.http.post("http://localhost:3000/birds", birddata)
+    return this.http.post("https://667d5510297972455f64a257.mockapi.io/api/v1/birds", birddata)
   }
   updateBird(id:any, birddata:Bird){
-    return this.http.put<Bird>("http://localhost:3000/birds/"+id, birddata)
+    return this.http.put<Bird>("https://667d5510297972455f64a257.mockapi.io/api/v1/birds/"+id, birddata)
   }
   deleteBird(id:any){
-    return this.http.delete("http://localhost:3000/birds/"+id)
+    return this.http.delete("https://667d5510297972455f64a257.mockapi.io/api/v1/birds/"+id)
   }
 }
